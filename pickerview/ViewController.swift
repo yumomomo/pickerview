e01a7092d52231a86557daf03ed8c8f018b42802
yumomomo <chowyumo@gmail.com>
2014-12-15 23:23:55 +0800

    pickerviewdemo

--- pickerview/ViewController.swift
+++ pickerview/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    var GIRLFRIEND = ["VIVI", "CICI", "ANGELBB", "BIBI"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        itemLabel.text = GIRLFRIEND[0]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+        return 1
+        }
+        func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+            return GIRLFRIEND[row]
+        }
+        func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String!{
+            return GIRLFRIEND[row]
+        }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        var itemSelected = GIRLFRIEND[row]
+        itemLabel.text = itemSelected
+    }
 }
 
